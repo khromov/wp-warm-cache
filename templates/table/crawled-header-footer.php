c678e0ad4dd8a2d8f78ba7231b1a41ac89b52e73
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2013-06-26 22:02:06 +0200

    Cleaning out assets branch

--- templates/table/crawled-header-footer.php
+++
@@ -1,6 +0,0 @@
-<tr>
-	<th class="manage-column">Crawled at</th>
-	<th class="manage-column">Crawl time</th>
-	<th class="manage-column">Number of pages</th>
-	<th class="manage-column">Average load time per page</th>
-</tr>
